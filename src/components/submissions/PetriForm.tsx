65bd9f83f485a9c53206509f6cd4a0a163474bc5
putYourWifeOuttaWork <weisbergmm+1@gmail.com>
2025-06-21 17:14:05 -0400

    Updated PetriForm.tsx

--- src/components/submissions/PetriForm.tsx
+++ src/components/submissions/PetriForm.tsx
@@ -288,7 +288,8 @@ const PetriForm = forwardRef<PetriFormRef, PetriFormProps>(({
     <div id={id} className="border border-gray-200 rounded-lg p-3 bg-gray-50" data-testid={`petri-form-${formId}`}>
       <div className="flex justify-between items-center mb-2">
         <div className="flex items-center">
-          <h4 className="font-medium text-gray-900">Petri Sample #{formik.values.petriCode}</h4>
+          <h4 className="font-medium text-gray-900">Code {formik.values.petriCode}</h4>
+          <p>Click on the image uploader, select "Take a Photo" and snap the pic. No need to upload anything at all.</p>
           {/* Toggle expand/collapse button */}
           <button 
             type="button"
